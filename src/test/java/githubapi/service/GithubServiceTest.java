159df892154db2c5e449b9779496a59977f8971c
igor <Rikanskey>
2020-12-01 03:22:22 +0300

    Fifth step

--- src/test/java/githubapi/service/GithubServiceTest.java
+++ src/test/java/githubapi/service/GithubServiceTest.java
@@ -3,6 +3,7 @@ package githubapi.service;
 import githubapi.dto.OwnerResponse;
 import githubapi.dto.RepositoryResponse;
 import githubapi.repository.Repository;
+import githubapi.service.impl.GithubServiceImpl;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -16,14 +17,14 @@ public class GithubServiceTest extends Assert{
 
     @Test
     public void createService(){
-        GithubService githubService = new GithubService(repository);
-        assertNotNull(githubService);
+        GithubServiceImpl githubServiceImpl = new GithubServiceImpl(repository);
+        assertNotNull(githubServiceImpl);
     }
 
     @Test
     public void getUserRepositoriesTest(){
 
-        GithubService githubService = new GithubService(repository);
+        GithubServiceImpl githubServiceImpl = new GithubServiceImpl(repository);
         Mockito.when(repository.getRepositories("Rikanskey")).thenReturn(Arrays.asList(
                 RepositoryResponse
                    .builder()
@@ -40,7 +41,14 @@ public class GithubServiceTest extends Assert{
                         .owner(OwnerResponse.builder().login("Rikanskey").id(1L).build())
                         .build()
       ));
-        List<RepositoryResponse> userRepositories = githubService.getUserRepositories("Rikanskey");
+        List<RepositoryResponse> userRepositories = githubServiceImpl.getUserRepositories("Rikanskey");
         assertEquals(2, userRepositories.size());
+    }
+
+    @Test
+    public void getUserRepositoriesGithubApiTest(){
+        GithubService githubServiceImpl = new GithubServiceImpl(new Repository("https://api.github.com"));
+        List<RepositoryResponse> userRepositories = githubServiceImpl.getUserRepositories("vladimir-polyakov");
+        assertEquals(10, userRepositories.size());
     }
 }
